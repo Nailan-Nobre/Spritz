79159baf4b6b21c23e52eab81782f6037460dd89
Nailan-Nobre <nailannobre22@gmail.com>
2025-11-12 16:20:55 -0300

    teste 2 de commit

--- src/modules/lr/slr_from_scratch.py
+++ src/modules/lr/slr_from_scratch.py
@@ -1,13 +1,12 @@
 from typing import Any
 
+
 class SimpleLinearRegressionFS:
-    
     def __init__(self) -> None:
-        """Classe que implementa minha regressão simples
-        """
+        """Classe que implementa minha regressão simples"""
         self.__b0 = None
         self.__b1 = None
-    
+
     def train(self) -> Any:
         """Método para treinar o modelo, usa o método Ordinary Least Squares
 
@@ -16,10 +15,9 @@ class SimpleLinearRegressionFS:
         """
         # Implementar o OLS aqui!!
         return None
-    
+
     def get_b0(self) -> Any:
         return self.__b0
-    
+
     def get_b1(self) -> Any:
         return self.__b1
-    
